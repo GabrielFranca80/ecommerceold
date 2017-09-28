568c542449903b94f9448f1dcdd5a03e9e943d97
noir <gabriel.franca@gmail.com>
2017-09-28 16:14:18 -0300

    Tela de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -10,6 +10,8 @@ class Page
 	private $tpl;
 	private $options = [];
 	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
 		"data"=>[]
 	];
 	
@@ -30,7 +32,7 @@ class Page
 	
 	$this->setData($this->options["data"]);
 	
-	$this->tpl->draw("header");
+	if ($this->options["header"] === true) $this->tpl->draw("header");
 
 	}
 
@@ -55,7 +57,7 @@ class Page
 	public function __destruct()
 	{
 
-		$this->tpl->draw("footer");
+		if ($this->options["footer"] === true) $this->tpl->draw("footer");
 	}
 }
 ?>
